9b741c428d1de9e8ead2f1912a20da090ad36fbc
Ian Simpson <ianbsimpson@gmail.com>
2022-10-20 12:33:44 -0700

    first commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Write a number thats greater than 9");
+
+            var answer = int.Parse(Console.ReadLine());
+
+            string response;
+
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is not greater than 9.");
+            }
+            else
+            {
+                Console.WriteLine($"Yess!!! {answer} is greater than or equal to nine!!!");
+            }
+
+            //var answer = 4;
+
+            //var response = (answer < 9) ? " is less than nine" : " is greater than or equal to nine"; 
+            
+
+        
         }
+       
     }
 }
